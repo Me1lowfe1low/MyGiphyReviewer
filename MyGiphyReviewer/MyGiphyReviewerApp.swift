c8fa6e0cc7c1bf1ebaeb07bb4d2c3c2bdf130b82
C'est une pomme <dmgordienko@gmail.com>
2023-04-19 11:10:31 +0400

    Initial Commit

---
+++ MyGiphyReviewer/MyGiphyReviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGiphyReviewerApp.swift
+//  MyGiphyReviewer
+//
+//  Created by Дмитрий Гордиенко on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyGiphyReviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
